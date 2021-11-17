4a7fa30e0fc32b981b45c807dae9d334e3e1ef9e
beslimir <marin.beslo@gmail.com>
2021-11-17 11:10:14 +0100

    Add model and dto

---
+++ app/src/main/java/com/example/mydictionary/feature_dictionary/data/remote/DictionaryAPI.kt
@@ -0,0 +1,12 @@
+package com.example.mydictionary.feature_dictionary.data.remote
+
+import com.example.mydictionary.feature_dictionary.data.remote.dto.WordInfoDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface DictionaryAPI {
+
+    @GET("/api/v2/entries/en/{word}")
+    suspend fun getWordInfo(@Path("word") word: String): List<WordInfoDto>
+
+}
---
+++ app/src/main/java/com/example/mydictionary/feature_dictionary/data/remote/dto/PhoneticDto.kt
@@ -0,0 +1,6 @@
+package com.example.mydictionary.feature_dictionary.data.remote.dto
+
+data class PhoneticDto(
+    val audio: String,
+    val text: String
+)
